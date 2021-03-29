ee62b9221e431bbf7d6e61c3422e0417186ccd39
PdoubleU <piotrwitasik1988@gmail.com>
2021-03-29 17:57:55 +0200

    [refactor]: prepare files to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactFCCtest from 'react-fcctest';
 import TextInput from './components/input_output.js';
 
 function App() {
   return (
     <div className="App">
       <TextInput/>
-      <ReactFCCtest/>
     </div>
   );
 }
--- src/components/placeholder.js
+++ src/components/placeholder.js
@@ -44,10 +44,7 @@ And here. | Okay. | I think we get it.
 * And last but not least, let's not forget embedded images:
 
 
-**Made by Piotr Witasik as certification project on FreeCodeCamp**
-
-
-![FreeCodeCamp Logo](https:////discourse-user-assets.s3.amazonaws.com/original/2X/c/cf6e8e0a75acb7aa900eb304270a6407ae1d9e2c.jpg)`;
+**Made by Piotr Witasik as certification project on FreeCodeCamp**`;
 
 
 
