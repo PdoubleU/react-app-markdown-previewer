6db9c015f88a50c9762a291940808068d7a95134
PdoubleU <piotrwitasik1988@gmail.com>
2021-03-29 17:54:35 +0200

    Updates

---
+++ precache-manifest.df1e9a8647d9399eb0ac1bf4e795e18a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7cbe08e172939c707629199a48b2c3b",
+    "url": "/react_markdown_prev_exercise_04_2020/index.html"
+  },
+  {
+    "revision": "72995d5dc35e6484b534",
+    "url": "/react_markdown_prev_exercise_04_2020/static/css/main.b0fee5dd.chunk.css"
+  },
+  {
+    "revision": "9a9b9e4371d36e286c48",
+    "url": "/react_markdown_prev_exercise_04_2020/static/js/2.71616c9a.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/react_markdown_prev_exercise_04_2020/static/js/2.71616c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72995d5dc35e6484b534",
+    "url": "/react_markdown_prev_exercise_04_2020/static/js/main.f6210795.chunk.js"
+  },
+  {
+    "revision": "89a2907c40a1685510a9",
+    "url": "/react_markdown_prev_exercise_04_2020/static/js/runtime-main.31827a6c.js"
+  }
+]);
